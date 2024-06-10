c1fe3510ad66cae3afd1a3e3e74b5313d45109f7
Phan Tấn Dũng <dungpt@vinades.vn>
2024-06-10 17:55:00 +0700

    Update scripts/build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -16,10 +16,11 @@ DIR_PATH=$PWD
 
 npm run admin-css
 
-curl -k "https://raw.githubusercontent.com/twbs/bootstrap/v5.3.3/dist/js/bootstrap.bundle.min.js" > "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js"
-curl -k "https://raw.githubusercontent.com/twbs/bootstrap/v5.3.3/dist/js/bootstrap.bundle.min.js.map" > "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js.map"
+rm -f "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js"
+rm -f "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js.map"
 
-#sed -i 's/bootstrap.bundle.min.js.map/\/themes\/admin_future\/js\/bootstrap.bundle.min.js.map/g' $DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js
+cp "$DIR_PATH/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js" "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js"
+cp "$DIR_PATH/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map" "$DIR_PATH/src/themes/admin_future/js/bootstrap.bundle.min.js.map"
 
 find "$DIR_PATH/src/themes/admin_future/webfonts" -type f -name "fa-*" | xargs /bin/rm -f
 cp -r "$DIR_PATH/node_modules/@fortawesome/fontawesome-free/webfonts/." "$DIR_PATH/src/themes/admin_future/webfonts/"
