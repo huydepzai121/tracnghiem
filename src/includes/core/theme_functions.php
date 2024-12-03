86ef56771b8ed76fdd3691abe2e268af844f4b02
Phan Tấn Dũng <dungpt@vinades.vn>
2024-12-03 11:26:18 +0700

    Move the info_die handling file to the theme

--- src/includes/core/theme_functions.php
+++ src/includes/core/theme_functions.php
@@ -97,51 +97,14 @@ function nv_info_die($page_title, $info_title, $info_content, $error_code = 200,
     }
     !empty($global_config['module_theme']) && $dir_basenames[] = $global_config['module_theme'];
     !empty($global_config['site_theme']) && $dir_basenames[] = $global_config['site_theme'];
-    $template = get_tpl_dir($dir_basenames, 'default', '/system/info_die.tpl');
-    $tpl_path = NV_ROOTDIR . '/themes/' . $template . '/system';
 
     empty($global_config['site_url']) && $global_config['site_url'] = NV_SERVER_PROTOCOL . '://' . $global_config['my_domains'][0] . NV_SERVER_PORT;
     empty($global_config['site_logo']) && $global_config['site_logo'] = NV_ASSETS_DIR . '/images/logo.png';
 
-    $xtpl = new XTemplate('info_die.tpl', $tpl_path);
-    $xtpl->assign('SITE_CHARSET', $global_config['site_charset']);
-    $xtpl->assign('PAGE_TITLE', $page_title);
-    $xtpl->assign('HOME_LINK', $global_config['site_url']);
-    $xtpl->assign('LANG', \NukeViet\Core\Language::$lang_global);
-    $xtpl->assign('TEMPLATE', $template);
-    $xtpl->assign('SITE_NAME', empty($global_config['site_name']) ? '' : $global_config['site_name']);
-
-    $site_favicon = NV_BASE_SITEURL . 'favicon.ico';
-    if (!empty($global_config['site_favicon']) and file_exists(NV_ROOTDIR . '/' . $global_config['site_favicon'])) {
-        $site_favicon = NV_BASE_SITEURL . $global_config['site_favicon'];
-    }
-    $xtpl->assign('SITE_FAVICON', $site_favicon);
-
-    $xtpl->assign('LOGO', NV_BASE_SITEURL . $global_config['site_logo']);
-    $xtpl->assign('INFO_TITLE', $info_title);
-    $xtpl->assign('INFO_CONTENT', $info_content);
-
-    if (defined('NV_IS_ADMIN') and !empty($admin_link)) {
-        $xtpl->assign('ADMIN_LINK', $admin_link);
-        $xtpl->assign('GO_ADMINPAGE', empty($admin_title) ? $nv_Lang->getGlobal('admin_page') : $admin_title);
-        $xtpl->parse('main.adminlink');
-    }
-    if (!empty($site_link)) {
-        $xtpl->assign('SITE_LINK', $site_link);
-        $xtpl->assign('GO_SITEPAGE', empty($site_title) ? $nv_Lang->getGlobal('go_homepage') : $site_title);
-        $xtpl->parse('main.sitelink');
-    }
-
-    if ($error_code >= 400) {
-        $xtpl->parse('main.is_error');
-    } else {
-        $xtpl->parse('main.is_info');
-    }
-
-    $xtpl->parse('main');
+    $php_dir = get_tpl_dir($dir_basenames, 'default', '/theme_info_die.php');
 
     include NV_ROOTDIR . '/includes/header.php';
-    $xtpl->out('main');
+    echo require NV_ROOTDIR . '/themes/' . $php_dir . '/theme_info_die.php';
     include NV_ROOTDIR . '/includes/footer.php';
 }
 
