51dbc3e107bdc8bbd51cf0fadb8e1f2abb380f0e
Phan Tấn Dũng <dungpt@vinades.vn>
2025-01-15 14:03:57 +0700

    The requirement for PHP 8.2 for NukeViet CMS 5.0

--- src/includes/mainfile.php
+++ src/includes/mainfile.php
@@ -155,7 +155,6 @@ require NV_ROOTDIR . '/includes/utf8/' . $sys_info['string_handler'] . '_string_
 require NV_ROOTDIR . '/includes/utf8/utf8_functions.php';
 require NV_ROOTDIR . '/includes/core/filesystem_functions.php';
 require NV_ROOTDIR . '/includes/functions.php';
-require NV_ROOTDIR . '/includes/new_functions.php';
 require NV_ROOTDIR . '/includes/core/theme_functions.php';
 
 // IP Ban
